ac943a11cf58f4febaa873fa1f87427254205250
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-04 16:24:03 +0100

    Primo esercizio  "Mail"

---
+++ script/script.js
@@ -0,0 +1,28 @@
+// Mail Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Non è necessario provvedere alla validazione delle email
+
+let mail = prompt("inserisci la tua mail");
+let lista = [
+  "orsouane@gmail.com",
+  "elaouizeb@gmail.com",
+  "classe136@gmail.com",
+];
+if (mail.includes("@") && mail.length > 0) {
+  let i = 0;
+
+  while (i < lista.length) {
+    if (lista[i] === mail) {
+      console.log("sei già registrato");
+      break;
+    }
+    i++;
+  }
+
+  if (lista[i] != mail && i == lista.length) {
+    console.log("Non sei registrato, devi creare un account");
+  }
+} else {
+  console.log("Controlla la tua mail");
+}
